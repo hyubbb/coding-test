ec49ff8dda13c8867a86f95daa904bc231e82589
프론트엔드 개발자 이창협 <32926006+hyubbb@users.noreply.github.com>
2025-03-27 23:17:29 +0900

    [level 0] Title: 다항식 더하기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,22 +1,21 @@
 function solution(s) {
   const answer = [];
-  const b = s.split(" ").filter((v) => v !== "+");
-  const c = b.filter((v) => v.includes("x"));
-  const d = b.filter((v) => !v.includes("x"));
-  const e = c.reduce((acc, cur) => {
-    if (cur.length > 1) {
-      acc += +cur.split("x")[0];
-    } else {
-      acc += 1;
-    }
-    return acc;
-  }, 0);
+  const b = s.split(" + ");
 
-  if (e === 1) answer.push("x");
-  else if (e > 1) answer.push(e + "x");
+  const xSum = b
+    .filter((v) => v.includes("x"))
+    .reduce((acc, cur) => {
+      acc += cur.length > 1 ? +cur.split("x")[0] : 1;
+      return acc;
+    }, 0);
 
-  const constantSum = d.reduce((acc, cur) => acc + parseInt(cur), 0);
-  if (constantSum > 0) answer.push(constantSum);
+  xSum && answer.push(xSum == 1 ? "x" : xSum + "x");
+
+  const numSum = b
+    .filter((v) => !v.includes("x"))
+    .reduce((acc, cur) => acc + parseInt(cur), 0);
+
+  numSum && answer.push(numSum);
 
   return answer.join(" + ");
 }
