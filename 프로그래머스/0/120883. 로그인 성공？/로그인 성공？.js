caa6c38c933d8bfda508faf43e3413803df15c42
프론트엔드 개발자 이창협 <32926006+hyubbb@users.noreply.github.com>
2025-04-05 22:45:07 +0900

    [level 0] Title: 로그인 성공?, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -2,10 +2,16 @@ function solution(id_pw, db) {
 
     const [id,pw] = id_pw
     
-    const match = db.filter((a)=>{
-        return a[0]==id;
-    })[0]
+    // 1차답안 O(n)
+    // const match = db.filter((a)=>{
+    //     return a[0]==id;
+    // })[0]
     
-    return !match ? "fail" : pw==match[1] ? "login" : "wrong pw";
+    // filter보다 find를 썻어야했다.
+    
+    // 2차 O(1)
+    const match = new Map(db);
+     
+    return match.has(id) ? match.get(id)==pw ? "login" : "wrong pw" : "fail"
     
 }
