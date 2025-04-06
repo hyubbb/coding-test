9e37ad8e2de9aaf4476c2ca00369fab166866331
프론트엔드 개발자 이창협 <32926006+hyubbb@users.noreply.github.com>
2025-04-06 20:41:46 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 19.62 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,6 +1,15 @@
 function solution(s) {    
+    // 1번째 방법 O(n2) 효율최악
+    // return [...s].map((a,idx)=>{
+    //     let pos = s.slice(0,idx).lastIndexOf(a);
+    //     return pos<0? pos: idx-pos
+    // })
+    
+    let hash = {};
+     
     return [...s].map((a,idx)=>{
-        let pos = s.slice(0,idx).lastIndexOf(a);
-        return pos<0? pos: idx-pos
-    })
+       let result =  hash[a] !== undefined ? idx-hash[a]: -1;
+        hash[a] = idx;
+        return result;
+     })
 }
