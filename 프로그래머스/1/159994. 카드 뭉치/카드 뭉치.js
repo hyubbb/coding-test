da21ef30f799401c704e721abbdb6936d1e1471c
프론트엔드 개발자 이창협 <32926006+hyubbb@users.noreply.github.com>
2025-04-23 22:31:09 +0900

    [level 1] Title: 카드 뭉치, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,15 +1,15 @@
 function solution(cards1, cards2, goal) {
- 
-    for(const v of goal){
-        
-        if(cards1[0] == v){
-            cards1.shift();
-        }else if(cards2[0]==v){
-            cards2.shift();
-        }else{
-            return "No"
+    let i = 0, j = 0;
+
+    for (const word of goal) {
+        if (i < cards1.length && cards1[i] === word) {
+            i++;
+        } else if (j < cards2.length && cards2[j] === word) {
+            j++;
+        } else {
+            return "No";
         }
     }
-    return "Yes"
-    
+
+    return "Yes";
 }
