3893555e5643562f7d882f08c4abddea12fb8ae1
프론트엔드 개발자 이창협 <32926006+hyubbb@users.noreply.github.com>
2025-05-08 09:37:49 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 4.12 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,12 +1,4 @@
 function solution(left, right) {
-  let answer = 0;
-  for (let i = left; i <= right; i++) {
-    let count = 0;
-    for (let j = 1; j <= i; j++) {
-      if (i % j == 0) count += 1;
-    }
-    if (count % 2 == 0) answer += i;
-    else answer -= i;
-  }
-  return answer;
+    var answer = 0;
+    return answer;
 }
