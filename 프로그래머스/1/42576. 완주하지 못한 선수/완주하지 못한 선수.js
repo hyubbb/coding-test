3b1f0bfb1a445b1e6fc56857687d035df9467d3b
프론트엔드 개발자 이창협 <32926006+hyubbb@users.noreply.github.com>
2025-05-08 22:28:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 34.64 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,17 +1,18 @@
 function solution(participant, completion) {
   const map = new Map();
 
-  for (let i = 0; i < participant.length; i++) {
-    let a = participant[i];
-    let b = completion[i];
-    map.set(a, (map.get(a) || 0) + 1);
-    map.set(b, (map.get(b) || 0) - 1);
-  }
- 
+    for(let i=0; i<participant.length;i++){
+        let a= participant[i];
+        let b= completion[i];
+        map.set(a, (map.get(a)||0)+1);
+        map.set(b, (map.get(b)||0)-1);
+    }
+    
+    for(let [a,b] of map){
 
-  for (let [key, value] of map) {
-    if (value > 0) return key;
-  }
-
-  return "";
+        if(b>0) return a;
+    }
+    
+    return ""
+    
 }
