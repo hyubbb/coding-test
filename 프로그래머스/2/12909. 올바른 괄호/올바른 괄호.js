3529b7b1aec97c6ace0acf1e6a7a7b4bc05bf8f1
프론트엔드 개발자 이창협 <32926006+hyubbb@users.noreply.github.com>
2025-05-09 18:05:53 +0900

    [level 2] Title: 올바른 괄호, Time: 32.33 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,13 +1,12 @@
 function solution(s) {
     const answer=[];
-    for(let i=0; i<s.length; i++){
-        if(s[i]=="("){
-            answer.push(s[i]);
+    for(const v of s){
+        if(v=="("){
+            answer.push(v);
         }else{
             if(answer.length == 0) return false;
             answer.pop();
         }
     }
-    console.log(answer)
     return answer.length == 0 ? true : false
 }
