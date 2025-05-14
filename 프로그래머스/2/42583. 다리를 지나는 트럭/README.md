f17490d9163ddb394151a5bb722a65d5fad54f63
프론트엔드 개발자 이창협 <32926006+hyubbb@users.noreply.github.com>
2025-05-14 14:11:34 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 59.51 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 37.8 MB, 시간: 28.59 ms
+메모리: 38 MB, 시간: 59.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 10일 22:04:38
+2025년 05월 14일 14:11:31
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,19 +1,23 @@
 function solution(bridge_length, weight, truck_weights) {
   let time = 0;
-  let bridge = Array(bridge_length).fill(0); // 다리 상태 초기화
+
+  const bridge = Array(bridge_length).fill(0);
   let totalWeight = 0;
 
-  while (truck_weights.length > 0 || totalWeight > 0) {
+  while (totalWeight > 0 || truck_weights.length > 0) {
     time++;
 
-    totalWeight -= bridge.shift();
+    const outTruck = bridge.shift();
     const nextTruck = truck_weights[0];
-    if (nextTruck && totalWeight + nextTruck <= weight) {
-      bridge.push(truck_weights.shift());
-      totalWeight += nextTruck;
+    totalWeight -= outTruck;
+
+    if (totalWeight + nextTruck <= weight) {
+      const truck = truck_weights.shift();
+      bridge.push(truck);
+      totalWeight += truck;
     } else {
       bridge.push(0);
     }
   }
-  return time;
+    return time;
 }
